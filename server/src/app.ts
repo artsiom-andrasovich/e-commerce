8e0d1e22f10074494d15c9b1a6c86c1b11b60b32
artsiom <pos2.ebasha@gmail.com>
2026-05-22 11:25:26 +0200

    add helmet and cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,21 @@
 import { router as categoriesRoutes } from '@categories/categories.route';
 import { errorMiddleware } from '@middlewares';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express, { Application } from 'express';
+import helmet from 'helmet';
+
+dotenv.config();
 
 const app: Application = express();
+
+app.use(helmet());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
